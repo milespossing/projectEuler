3a2de80250c3120804d65aab143e11e530ae5acf
Miles Possing <miles@VonNeuman.localdomain>
2021-10-15 15:48:26 -0700

    Rust p10

--- rust/pje/src/problems/mod.rs
+++ rust/pje/src/problems/mod.rs
@@ -1,7 +1,9 @@
-pub mod p2;
-pub mod p3;
+pub mod p002;
+pub mod p003;
+pub mod p010;
 
 pub fn run_all() {
-    println!("P2: {}", p2::run());
-    println!("P3: {}", p3::run());
+    println!("P2: {}", p002::run());
+    println!("P3: {}", p003::run());
+    println!("P10: {}", p010::run());
 }
---
+++ rust/pje/src/problems/p010.rs
@@ -0,0 +1,5 @@
+
+
+pub fn run() -> usize {
+    primal::Primes::all().take_while(|&x| x < 2000000).sum()
+}
