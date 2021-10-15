27a2d764bfaadb420584fffe0cd89a338157d99e
Miles Possing <miles@VonNeuman.localdomain>
2021-10-14 18:58:29 -0700

    Adding tests

--- rust/pje/src/main.rs
+++ rust/pje/src/main.rs
@@ -4,3 +4,18 @@ mod tools;
 fn main() {
     problems::run_all();
 }
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn p2() {
+        let output = crate::problems::p2::run();
+        assert_eq!(output, 4613732);
+    }
+
+    #[test]
+    fn p3() {
+        let output = crate::problems::p3::run();
+        assert_eq!(output, 6857);
+    }
+}
